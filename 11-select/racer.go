6b01ec2c982056c4b446062e32f23482b78c889f
Samuel Motta <samuelsamuca953@gmail.com>
2024-07-10 15:25:36 -0300

    Select

---
+++ 11-select/racer.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+var tenSecondTimeout = 10 * time.Second
+
+func Racer(a, b string) (string, error) {
+	return ConfigurableRacer(a, b, tenSecondTimeout)
+}
+
+func ConfigurableRacer(a, b string, timeout time.Duration) (string, error) {
+	// Case `ping(a)` terminar primeiro, retorna `a`
+	select {
+	case <-ping(a):
+		return a, nil
+	case <-ping(b):
+		return b, nil
+	case <-time.After(timeout):
+		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
+	}
+}
+
+func ping(url string) chan struct{} {
+	// Why struct{} and not another type like a bool?
+	// Well, a chan struct{} is the smallest data type available from a memory perspective
+	// so we get no allocation versus a bool.
+	// Since we are closing and not sending anything on the chan, why allocate anything?
+	ch := make(chan struct{})
+
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+
+	return ch
+}
